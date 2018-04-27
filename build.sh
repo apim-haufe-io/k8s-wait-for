c09feaf06477b179de204262325b797096a9c312
Martin Danielsson <martin@danielsson.de>
2018-04-27 22:33:53 +0200

    Fix build

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ echo "============================================"
 echo "Building image..."
 echo "============================================"
 
-git log -1 --decorate=short > ./git_last_commit
-git rev-parse --abbrev-ref HEAD > ./git_branch
+git log -1 --decorate=short > ./src/git_last_commit
+git rev-parse --abbrev-ref HEAD > ./src/git_branch
 
 gitRef=$(git rev-parse HEAD)
 buildDate=$(date -u +'%Y-%m-%d %H:%M:%S UTC')
